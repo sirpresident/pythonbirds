7824695ce636a8449991a0652b9a8837d1d6e416
sirpresident <richardnixon@posteo.org>
2019-02-22 00:38:20 -0300

    Criado atributos complexo Filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=29):
+    def __init__(self, *filhos, nome=None, idade=29):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Nixon')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Richard'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='renzo')
+    nixon = Pessoa(renzo, nome='Nixon')
+    print(Pessoa.cumprimentar(nixon))
+    print(id(nixon))
+    print(nixon.cumprimentar())
+    print(nixon.nome)
+    print(nixon.idade)
+    for filho in nixon.filhos:
+        print(filho.nome)
