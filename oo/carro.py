427e344b1dd19ba043c00e9c96e5bffadb7bd23f
Richard Nixon <richardnixon@posteo.org>
2019-03-21 22:12:00 -0300

    Carro utilizando os componentes Motor e Direção implementados

--- oo/carro.py
+++ oo/carro.py
@@ -98,6 +98,27 @@ Exemplo:
 
 
 """
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+    def acelerar(self):
+        self.motor.acelerar()
+    def frear(self):
+        self.motor.frear()
+    def calcular_direcao(self):
+        return self.direcao.valor
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
+
+
 class Motor:
     def __init__(self):
         self.velocidade = 0
@@ -129,7 +150,3 @@ class Direcao:
 
     def girar_a_esquerda(self):
         self.valor = self.rotacao_a_esquerda [self.valor]
-
-class Carro:
-    def calcular_velocidade(self):
-        pass
